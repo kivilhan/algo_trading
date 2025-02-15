34acb10dab5e6cb74aac4e071bd26b18bd0ec0a6
tnsChiller <ikivanc4@gmail.com>
2025-02-15 19:32:54 +0300

    -

--- data_pipes.py
+++ data_pipes.py
@@ -39,4 +39,49 @@ def nova_data(symbols, df):
     y_norm = y_np * 50
 
     # return {"x": x_std, "y": y_std}
-    return {"x": x_norm, "y": y_norm, "index": index} 
+    return {"x": x_norm, "y": y_norm, "index": index}
+
+def hanzo_df_array(symbols, df):
+    close = df["Close"]
+    vol = df["Volume"]
+
+    # Check NaN ratios
+    for sym in symbols:
+        sym_close = df["Close"][sym].to_numpy()
+        sym_vol = df["Volume"][sym].to_numpy()
+        nan_ratio_close = np.isnan(sym_close).sum() / len(sym_close)
+        nan_ratio_vol = np.isnan(sym_vol).sum() / len(sym_vol)
+        nan_ratio = max(nan_ratio_close, nan_ratio_vol)
+        if nan_ratio > 0.1:
+            print(f"{sym} nan ratio: {nan_ratio:.4f} - dropping")
+            close = close.drop(sym, axis=1)
+            vol = vol.drop(sym, axis=1)
+
+    # Convert to np array
+    close_np, vol_np = close.to_numpy(), vol.to_numpy()
+    hist, future = 35, 7
+    x, y = [], []
+
+    for idx in range(hist+1, len(close_np)-future):
+        p0 = close_np[idx - hist - 1:idx - 1]
+        p1 = close_np[idx - hist:idx]
+        p_idx = p1 / p0 - 1
+
+        v0 = vol_np[idx - hist - 1:idx - 1]
+        v1 = vol_np[idx - hist:idx]
+        v_idx = v1 / v0 - 1
+
+        x_idx = np.stack((p_idx.T, v_idx.T), axis=2)
+
+        y0 = close_np[idx]
+        y1 = close_np[idx + future]
+        y_idx = np.array(([y1 / y0 - 1])).T
+
+        if not np.any(np.isnan(x_idx)) and not np.any(np.isnan(y_idx)):
+            x.append(x_idx)
+            y.append(y_idx)
+
+    x_np = np.stack(x, axis=0)
+    y_np = np.stack(y, axis=0)
+
+    return { "x": x_np, "y": y_np }
--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,12 @@
 import numpy as np
+import torch
+from matplotlib import pyplot
 
-def score_0(y, mod_out):
-    
+def plt_single_t_multi_sym(mod, x, y):
+    pred = mod(x)
+
+
+def score_0(model, dict_path, data):
+    model.load_state_dict(torch.load(dict_path, weights_only=True))
+    x, y = data["x"], data["y"]
+    mod_out = model(x)
