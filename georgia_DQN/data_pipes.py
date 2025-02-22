8e88bd6fb7ccb12b7b3e99a297278d0a0dc1ebe6
kivilhan <ikivanc4@gmail.com>
2025-02-22 18:18:32 +0300

    -

--- georgia_DQN/data_pipes.py
+++ georgia_DQN/data_pipes.py
@@ -7,7 +7,7 @@ def process_df(df_path, win_ftr=20, win_past=20, gamma=0.9, offset=50):
     # Columns that will be normalized each window
     window_norm = [
         'Open', 'High', 'Low', 'Close', 'Adj Close','SMA_10',
-        'SMA_50', 'EMA_10', 'EMA_50'
+        'SMA_50', 'EMA_10', 'EMA_50', 
     ]
 
     # Columns that will be normalized throughout whole history
@@ -43,5 +43,58 @@ def process_df(df_path, win_ftr=20, win_past=20, gamma=0.9, offset=50):
     df_norm = np.concatenate((window_norm_np, hist_norm_np), axis=1)
     stack_list_2 = [df_norm[i:len(df_norm) - win_past + i] for i in range(win_past)]
     mod_ins = np.stack(stack_list_2, axis=1)
+
+    return {"x": mod_ins, "y": rewards_norm, "close": close_trim}
+
+def process_df_2(df_path, win_ftr=20, win_past=20, gamma=0.9, offset=50):
+    processed_df = pd.read_csv(df_path)[offset:]
+
+    # Columns that will be normalized each window
+    window_norm = [
+        'Open', 'High', 'Low', 'Close', 'Adj Close','SMA_10',
+        'SMA_50', 'EMA_10', 'EMA_50', 
+    ]
+
+    # Columns that will be normalized throughout whole history
+    hist_norm = [
+        'Volume', 'Doji', 'Engulfing', 'Hammer', 'Morning Star',
+        'Evening Star', 'RSI_14', 'MACD', 'MACD_Signal', 'MACD_Hist'
+    ]
+
+    close = ['Close']
+    window_np = processed_df[window_norm].to_numpy()
+    hist_np = processed_df[hist_norm].to_numpy()
+    close_np = processed_df[close].to_numpy()
+
+    change_np = close_np[1:] / close_np[:-1]
+    stack_list = [change_np[i:len(change_np) - win_ftr + i] for i in range(win_ftr)]
+    change_stack = np.concatenate(stack_list, axis=1)
+    discounts = np.array([[gamma**i for i in range(win_ftr)]])
+    change_discounts = (change_stack - 1) * discounts
+    rewards = change_discounts.sum(axis=1)
+    rewards = rewards.reshape((len(rewards), 1))[win_past:]
+    rewards_norm = rewards / np.abs(rewards).max()
+
+    win_trim = window_np[1:len(window_np) - win_ftr]
+    hist_trim = hist_np[1:len(window_np) - win_ftr]
+    close_trim = close_np[1 + win_past :len(window_np) - win_ftr]
+
+    win_list = [win_trim[i:len(win_trim) - win_past + i] for i in range(win_past)]
+    win_stack = np.stack(win_list, axis=1)
+    _min = win_stack.min(axis=1).reshape((win_stack.shape[0], 1, win_stack.shape[2]))
+    _max = win_stack.max(axis=1).reshape((win_stack.shape[0], 1, win_stack.shape[2]))
+    window_norm_np = (win_stack - _min) / (_max - _min)
+
+    _min = hist_trim.min(axis=1).reshape(hist_trim.shape[0], 1)
+    _max = hist_trim.max(axis=1).reshape(hist_trim.shape[0], 1)
+    hist_norm_np = (hist_trim - _min) / (_max - _min)
+    hist_list = [hist_norm_np[i:len(win_trim) - win_past + i] for i in range(win_past)]
+    mod_ins = np.stack(hist_list, axis=1)
+
+    return {"x": mod_ins, "y": rewards_norm, "close": close_trim}
+
+
+def process_df_3(df_path, win_ftr=20, win_past=20, gamma=0.9, offset=50):
+
 
     return {"x": mod_ins, "y": rewards_norm, "close": close_trim}
